52d4863151ef3b0035bfc5e124cd61621a995fdc
Jose Ibarra <jibarra@jibarras-iMac.local>
2016-08-03 16:25:34 -0600

    Changing project structure && Adding SASS preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+connect = require('gulp-connect'),
+sass = require('gulp-sass');
 
 gulp.task('connect', function() {
   connect.server({
-    //root: 'app', // no root = actual directory, with root = file(s) path to live reload
+    root: 'app', // no root = actual directory, with root = file(s) path to live reload
     livereload: true
   });
 });
 
 gulp.task('html', function () {
-  gulp.src('./*.html')
+  gulp.src('./app/*.html')
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./*.html'], ['html']);
+  gulp.watch(['./app/*.html'], ['html']);
 });
 
 gulp.task('default', ['connect', 'watch']);
+
+
+/*
+Material Used
+-https://scotch.io/tutorials/aesthetic-sass-1-architecture-and-style-organization
+-https://css-tricks.com/gulp-for-beginners/
+*/
