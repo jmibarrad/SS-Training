6827c6e13cfcab3f0f80a0e265cd4dc6741e87ad
Jose Ibarra <jibarra@jibarras-iMac.local>
2016-08-04 09:12:17 -0600

    Project Structure Fixed && SASS implemented

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
+'use strict';
+
 var gulp = require('gulp'),
 connect = require('gulp-connect'),
 sass = require('gulp-sass');
 
+var sass_src = './app/stylesheets/**/*.scss';
+var sass_dest = './app/stylesheets/css';
+var html_src = './app/*.html';
+
 gulp.task('connect', function() {
   connect.server({
-    root: 'app', // no root = actual directory, with root = file(s) path to live reload
+    root: 'app', // no root = actual directory, with root = file(s) path
     livereload: true
   });
 });
 
+gulp.task('sass', function () {
+  return gulp.src(sass_src)
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest(sass_dest));
+});
+
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
+  gulp.src(html_src)
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch([html_src, sass_src], ['html', 'sass']);
 });
 
 gulp.task('default', ['connect', 'watch']);
